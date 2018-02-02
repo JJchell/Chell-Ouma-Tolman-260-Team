1aa646cb4269c479f27c46eab05a342550cb61ef
scotttolman <scotttolman@gmail.com>
2018-02-01 23:13:25 -0600

    completed team activity

--- MormonTrail/src/mormontrail/MormonTrail.java
+++ MormonTrail/src/mormontrail/MormonTrail.java
@@ -5,6 +5,7 @@
  */
 package mormontrail;
 
+import  model.Character;
 /**
  *
  * @author TheChells
@@ -15,7 +16,27 @@ public class MormonTrail {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Character james = new Character();
+        
+        james.setName("James Chell");
+        james.setMoney(200);
+        james.setHuntingSkill(10);
+        james.setGatheringSkill(2);
+        james.setDailyStaminaDraw(5);
+        james.setCurrentHealth(8);
+        
+        
+        String jamesName = james.getName();
+        int jamesMoney = james.getMoney();
+        int jamesHunting = james.getHuntingSkill();
+        int jamesGathering = james.getGatheringSkill();
+        int jamesStamina = james.getDailyStaminaDraw();
+        int jamesHealth = james.getCurrentHealth();
+        
+        
+        System.out.println("Name = " + jamesName + "\nMoney = " + jamesMoney 
+                + "\nHunting skill = " + jamesHunting + "\nGather skill = " 
+                + jamesGathering + "\nStamina = " + jamesStamina 
+                + "\nHealth = " + jamesHealth);
     }
-    //Scott
 }
