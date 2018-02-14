c7b2787572313e1cc5f7c6ef1d2242b1d51320ab
scotttolman <scotttolman@gmail.com>
2018-02-13 22:59:04 -0600

    wrote huntSuccess() and added test

--- MormonTrail/src/control/ComplexCalculations.java
+++ MormonTrail/src/control/ComplexCalculations.java
@@ -10,5 +10,11 @@ package control;
  * @author scott
  */
 public class ComplexCalculations {
-    
+    public static int huntSuccess(int huntingSkill, int wildLife) {
+        if (huntingSkill > 0 && huntingSkill < 11 && wildLife > 0 && wildLife < 11) {
+            return huntingSkill * wildLife;
+        }
+        else
+            return -1; // returning negative number as indication of invalid arguments
+    }
 }
