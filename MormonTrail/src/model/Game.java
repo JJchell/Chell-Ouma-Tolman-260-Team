220d80ea74463cb39cd0fde010d1d9ef3a051d57
TheChells <TheChells@10.0.1.8>
2018-02-16 21:51:09 -0600

    basic project structure

---
+++ MormonTrail/src/model/Game.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author TheChells
+ */
+public class Game implements Serializable {
+    
+    private int daysOnTrail;
+    private int pace;
+    private int milesTraveled;
+    private int currentMoneyBalance;
+
+    public int getDaysOnTrail() {
+        return daysOnTrail;
+    }
+
+    public void setDaysOnTrail(int daysOnTrail) {
+        this.daysOnTrail = daysOnTrail;
+    }
+
+    public int getPace() {
+        return pace;
+    }
+
+    public void setPace(int pace) {
+        this.pace = pace;
+    }
+
+    public int getMilesTraveled() {
+        return milesTraveled;
+    }
+
+    public void setMilesTraveled(int milesTraveled) {
+        this.milesTraveled = milesTraveled;
+    }
+
+    public int getCurrentMoneyBalance() {
+        return currentMoneyBalance;
+    }
+
+    public void setCurrentMoneyBalance(int currentMoneyBalance) {
+        this.currentMoneyBalance = currentMoneyBalance;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + this.daysOnTrail;
+        hash = 29 * hash + this.pace;
+        hash = 29 * hash + this.milesTraveled;
+        hash = 29 * hash + this.currentMoneyBalance;
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" + "daysOnTrail=" + daysOnTrail + ", pace=" + pace 
+                + ", milesTraveled=" + milesTraveled + ", currentMoneyBalance=" 
+                + currentMoneyBalance + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Game other = (Game) obj;
+        if (this.daysOnTrail != other.daysOnTrail) {
+            return false;
+        }
+        if (this.pace != other.pace) {
+            return false;
+        }
+        if (this.milesTraveled != other.milesTraveled) {
+            return false;
+        }
+        if (this.currentMoneyBalance != other.currentMoneyBalance) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+}
---
+++ MormonTrail/src/model/Location.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author TheChells
+ */
+public class Location implements Serializable{
+    
+    private String type;
+    private int milepost;
+
+    public Location() {
+    
+    }
+    
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getMilepost() {
+        return milepost;
+    }
+
+    public void setMilepost(int milepost) {
+        this.milepost = milepost;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + Objects.hashCode(this.type);
+        hash = 97 * hash + this.milepost;
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" + "type=" + type + ", milepost=" + milepost + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Location other = (Location) obj;
+        if (this.milepost != other.milepost) {
+            return false;
+        }
+        if (!Objects.equals(this.type, other.type)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
+   
